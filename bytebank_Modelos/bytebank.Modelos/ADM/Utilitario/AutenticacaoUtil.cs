13d3fbb45a9a6ac097bb33c72cffe5224723c982
RuthMaria <ruth-15rms@hotmail.com>
2024-12-21 21:38:33 -0300

    first commit

---
+++ bytebank_Modelos/bytebank.Modelos/ADM/Utilitario/AutenticacaoUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace bytebank_Modelos.bytebank.Modelos.ADM.Utilitario
+{
+    internal class AutenticacaoUtil
+    {
+        public bool ValidarSenha(string senhaverdadeira, string senhatentativa)
+        {
+            return senhaverdadeira.Equals(senhatentativa);
+        }
+    }
+}
